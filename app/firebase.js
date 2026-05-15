d992b90ccc6f0c6e7138ea9af92efbdf45e0a97e
Waheed157 <malikkwaheedd@gmail.com>
2026-05-16 03:56:35 +0500

    Create firebase.js

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "dil-se-dating.firebaseapp.com",
+  projectId: "dil-se-dating",
+  storageBucket: "dil-se-dating.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
+export { signInWithPopup, onAuthStateChanged, signOut }
+dil-se-dating.firebaseapp.com
+dil-se-dating.firebaseapp.com

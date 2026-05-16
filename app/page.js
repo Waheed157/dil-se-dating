d664b343ecfc5b2022fbee2c925ad9fff8976f8f
Waheed157 <malikkwaheedd@gmail.com>
2026-05-16 07:24:02 +0500

    fix page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,10 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1>
-          Dil Se Dating ❤️
-        </h1>
-        <p className="text-2xl text-white mb-8">
-          Jahan Dil Milte Hain
-        </p>
-        <button className="bg-white text-pink-600 font-bold">
-          Shuru Karein
-        </button>
-      </div>
+      <h1>Dil Se Dating ❤️</h1>
+      <p>Jahan Dil Milte Hain</p>
     </main>
   );
 }
